75871172f859a0adcfcd18178f6b1d50b225a144
karrrtavy <ignogamer90@gmail.com>
2025-04-22 18:17:17 +0300

    fix

--- crossplatformprogramming/5/ind9.py
+++ crossplatformprogramming/5/ind9.py
@@ -1,22 +1,19 @@
-import numpy as np
-x = np.arange(-6, 7, 1)
-y1 = np.sqrt(1 + 2 * x**2 - np.sin(x)**2)
-y2 = (2 + x) / (np.sqrt(2) + np.exp(x))
-y = y1 + y2
-y_sorted = np.sort(y)
-min_y1 = np.min(y1)
-min_y2 = np.min(y2)
-if min_y1 < min_y2:
-    compare = "min(y1) < min(y2)"
-elif min_y1 > min_y2:
-    compare = "min(y1) > min(y2)"
-else:
-    compare = "min(y1) = min(y2)"
-print("x:", x)
-print("y1:", y1)
-print("y2:", y2)
-print("y = y1 + y2:", y)
-print("y (sorted):", y_sorted)
-print(f"min(y1) = {min_y1}")
-print(f"min(y2) = {min_y2}")
-print("Сравнение минимумов:", compare)
+# первые 16 предложений во второй файл. нужно посчитать кол-во гласных и кол-во согласных и вывести их кол-во.
+# в новый файл записать предложения из 1 файла, но каждое нечетное слово развернуть
+
+file2 = open(r'crossplatformprogramming/5/besi2.txt', 'w')
+
+with open(r'crossplatformprogramming/5/besi.txt', 'r', encoding='utf-8') as f:
+    data = f.read().strip()
+
+besi = list(map(str, data))
+# print(besi)
+
+pr1d6 = []
+count = 0
+
+while count < 16:
+    for i in besi:
+        if  i[len(i) - 1] == '.' or i[len(i) - 1] == '?':
+            count += 1
+
