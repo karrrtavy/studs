22ed9a282cc2aea138da9054acb8ed9fc4d792b0
karrrtavy <ignogamer90@gmail.com>
2025-05-26 11:56:24 +0300

    чтото

--- crossplatformprogramming/6/indind.py
+++ crossplatformprogramming/6/indind.py
@@ -79,13 +79,13 @@ def draw_star():
     t.up(); t.goto(xcor() - 25, ycor() - 125); t.down()
     talloval(100, -75)
     
-    t.up(); t.right(-20)
+    t.up(); t.right(-22)
     radius = 270
-    t.goto(-30, -radius + 70)
+    t.goto(-28, -radius + 70)
     t.down()
     angle = 180 - 180 / 5
     for i in range(5):
-        t.forward(radius * 2 * math.sin(math.pi / 5))
+        t.forward(radius * 2.1 * math.sin(math.pi / 5))
         t.left(angle)
 
 def on_click(x, y):
